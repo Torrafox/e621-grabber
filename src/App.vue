da4e04ac3736bac28851139deb8dc00efc1e5f2f
Torrafox <torrafox@gmail.com>
2021-09-01 18:12:39 +0200

    Set footer pos absolute

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
   <v-app :style="{ background: $vuetify.theme.themes[theme].background }">
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app dark color="primary">
       <div class="d-flex align-center">
         <h1 class="text-h5 font-weight-bold">
           e621-Grabber
         </h1>
       </div>
       <v-spacer></v-spacer>
-      <v-btn
-        :href='Urls.REPO'
-        target="_blank"
-        text
-      >
+      <v-btn text :href='Urls.REPO' target="_blank">
         <span class="mr-2">GitHub</span>
         <v-icon>mdi-github</v-icon>
       </v-btn>
     </v-app-bar>
-
     <v-main>
       <Core />
     </v-main>
-    <v-footer app padless>
+    <v-footer app absolute padless>
       <v-card flat tile max-width="600" color="transparent" class="text-center mx-auto">
         <v-card-text>
           This project is not affiliated, associated, authorized, endorsed by, or in any way officially connected with <a :href="Urls.E621" target="_blank">e621.net</a>, or any of its subsidiaries or its affiliates.
